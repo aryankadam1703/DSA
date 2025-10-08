f81fab2dd4703cee47b5111af878b98495da1fe3
Aryan Kadam <126709432+RnKadam@users.noreply.github.com>
2025-10-08 11:51:43 +0530

    Time: 2029 ms (37.91%) | Memory: 13.5 MB (16.14%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -5,18 +5,18 @@ class Solution(object):
         :type target: int
         :rtype: List[int]
         """
-        # for i in range(len(nums)-1):
-        #     for j in range(1,len(nums)):
-        #         if nums[i]+nums[j]==target:
-        #             return [i,j]
+        for i in range(len(nums)-1):
+            for j in range(i+1,len(nums)):
+                if nums[i]+nums[j]==target:
+                    return [i,j]
 
-        dictionary = {}
-        for i in range(len(nums)):
-            num = nums[i]
-            c = target - num
-            if c in dictionary:
-                return [dictionary[c], i]
-            dictionary[num] = i
-        return []
-            
-        
+
+        #Optimized solution
+
+        # hash_map = {} ---------------- Hashmap used
+        # for i, num in enumerate(nums):
+        #     complement = target - num
+        #     if complement in hash_map:
+        #         return [hash_map[complement], i]
+        #     hash_map[num] = i        
+        # return []
