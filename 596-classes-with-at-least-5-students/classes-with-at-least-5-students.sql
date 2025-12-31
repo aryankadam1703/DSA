155fc78cd0ac7872a9984cdcc2f1ad85bd1ef9c3
Aryan Kadam <126709432+aryankadam1703@users.noreply.github.com>
2025-12-31 16:35:05 +0530

    Time: 281 ms (95.58%) | Memory: 0B (100.00%) - LeetSync

--- 596-classes-with-at-least-5-students/classes-with-at-least-5-students.sql
+++ 596-classes-with-at-least-5-students/classes-with-at-least-5-students.sql
@@ -1,7 +1,4 @@
 # Write your MySQL query statement below
-with Limit_Class as (
-    select class from Courses
-    group by class
-    having count(student) >=5
-)
-select * from Limit_Class;
+select class from Courses
+group by class
+having count(*) >=5;
