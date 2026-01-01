2035728cf340f93feed04c8f60d71dad8b13fec6
Aryan Kadam <126709432+aryankadam1703@users.noreply.github.com>
2026-01-01 22:43:02 +0530

    Time: 665 ms (9.96%) | Memory: 0B (100.00%) - LeetSync

--- 1942-primary-department-for-each-employee/primary-department-for-each-employee.sql
+++ 1942-primary-department-for-each-employee/primary-department-for-each-employee.sql
@@ -1,11 +1,11 @@
 # Write your MySQL query statement below
 with Single_Dept_Emp as(
-    select employee_id,department_id,count(department_id) as cnt  from Employee
+    select employee_id,department_id from Employee
     group by employee_id
-    having cnt = 1
+    having count(department_id) = 1
 )
 
-select employee_id,department_id from Single_Dept_Emp
+select * from Single_Dept_Emp
 union
 select employee_id,department_id from Employee
 where primary_flag = 'Y'
