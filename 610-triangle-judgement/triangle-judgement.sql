177ff35e06723d52cff2056f51ce133124d006ae
Aryan Kadam <126709432+aryankadam1703@users.noreply.github.com>
2026-01-02 20:54:22 +0530

    Time: 262 ms (76.11%) | Memory: 0B (100.00%) - LeetSync

--- 610-triangle-judgement/triangle-judgement.sql
+++ 610-triangle-judgement/triangle-judgement.sql
@@ -1,14 +1,17 @@
 # Write your MySQL query statement below
 
-select x,y,z, 
-case 
-    when greatest(x,y,z) = x and y+z>x
-    then 'Yes'
-    when greatest(x,y,z) = y and x+z>y
-    then 'Yes'
-    when greatest(x,y,z) = z and x+y>z
-    then 'Yes'
-    else 'No'
-end as triangle
-from Triangle
+-- select x,y,z,                                    My Approach
+-- case 
+--     when greatest(x,y,z) = x and y+z>x
+--     then 'Yes'
+--     when greatest(x,y,z) = y and x+z>y
+--     then 'Yes'
+--     when greatest(x,y,z) = z and x+y>z
+--     then 'Yes'
+--     else 'No'
+-- end as triangle
+-- from Triangle
+
+
+select *, if(x+y>z and y+z>x and x+z>y,'Yes','No') as triangle from Triangle
 
