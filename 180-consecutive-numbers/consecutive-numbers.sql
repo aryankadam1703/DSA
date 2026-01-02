8b9e3cb2b256ff8c9c0fed71e10b42d5e5014603
Aryan Kadam <126709432+aryankadam1703@users.noreply.github.com>
2026-01-02 21:37:32 +0530

    Time: 494 ms (80.76%) | Memory: 0B (100.00%) - LeetSync

--- 180-consecutive-numbers/consecutive-numbers.sql
+++ 180-consecutive-numbers/consecutive-numbers.sql
@@ -9,11 +9,19 @@
 --   AND l1.id = l2.id - 1
 --   AND l2.id = l3.id - 1;
 
-with cte as (
-    select num,
-    lead(num,1) over() num1,
-    lead(num,2) over() num2
-    from logs
+-- with cte as (
+--     select num,
+--     lead(num,1) over() num1,
+--     lead(num,2) over() num2
+--     from logs
 
-)
-select distinct num ConsecutiveNums from cte where (num=num1) and (num=num2)
+-- )
+-- select distinct num ConsecutiveNums from cte where (num=num1) and (num=num2)
+
+SELECT DISTINCT l1.num AS ConsecutiveNums
+FROM Logs l1 
+join Logs l2
+on l1.id = l2.id+1
+join Logs l3
+on l1.id = l3.id+2
+WHERE l1.num = l2.num AND l2.num = l3.num
