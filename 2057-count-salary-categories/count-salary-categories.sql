57309929bea6948ca516bdbe97f44ef733ceb5d0
Aryan Kadam <126709432+aryankadam1703@users.noreply.github.com>
2026-01-03 22:31:43 +0530

    Time: 1610 ms (27.82%) | Memory: 0B (100.00%) - LeetSync

--- 2057-count-salary-categories/count-salary-categories.sql
+++ 2057-count-salary-categories/count-salary-categories.sql
@@ -1,22 +1,33 @@
 # Write your MySQL query statement below
-with low as(
-    select * from Accounts
-    where income<20000
-),
-average as(
-    select * from Accounts
-    where income >= 20000 and income<=50000
-),
-high as(
-    select * from Accounts
-    where income > 50000
-)
+-- with low as(
+--     select * from Accounts
+--     where income<20000
+-- ),
+-- average as(
+--     select * from Accounts
+--     where income >= 20000 and income<=50000
+-- ),
+-- high as(
+--     select * from Accounts
+--     where income > 50000
+-- )
+-- select 'Low Salary' as category,count(*) as accounts_count
+-- from low
+-- union
+-- select 'Average Salary' as category,count(*) as accounts_count
+-- from average
+-- union
+-- select 'High Salary' as category,count(*) as accounts_count
+-- from high
 
-select 'Low Salary' as category,count(*) as accounts_count
-from low
-union
-select 'Average Salary' as category,count(*) as accounts_count
-from average
-union
-select 'High Salary' as category,count(*) as accounts_count
-from high
+select 'Low Salary' as category,
+count(if(income<20000,1,NULL)) as accounts_count
+from Accounts
+union all
+select 'Average Salary' as category,
+count(if(income>=20000 and income<=50000,1,NULL)) as accounts_count
+from Accounts
+union all
+select 'High Salary' as category,
+count(if(income>50000,1,NULL)) as accounts_count
+from Accounts
