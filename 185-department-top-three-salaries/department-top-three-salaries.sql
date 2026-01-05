fe951a1c0f223c491c9aee301c1d63e5f7a3bfb1
Aryan Kadam <126709432+aryankadam1703@users.noreply.github.com>
2026-01-05 21:57:32 +0530

    Time: 1266 ms (18.00%) | Memory: 0B (100.00%) - LeetSync

--- 185-department-top-three-salaries/department-top-three-salaries.sql
+++ 185-department-top-three-salaries/department-top-three-salaries.sql
@@ -1,14 +1,27 @@
 # Write your MySQL query statement below
 
-with highest_earner as(
-    select d.name as Department,e.name as Employee,e.salary as Salary,
-    dense_rank() over (partition by d.name order by e.salary desc) as rnk
-    from Employee e
-    left join Department d
-    on e.departmentId=d.id    
-)
-select Department,Employee,Salary from highest_earner
-where rnk<=3;
+SELECT d.name AS Department, e.name AS Employee, e.salary AS Salary
+FROM Employee e
+JOIN Department d 
+ON e.departmentId = d.id
+WHERE(
+    SELECT COUNT(DISTINCT e2.salary)
+    FROM Employee e2
+    WHERE e2.departmentId = e.departmentId AND e2.salary >= e.salary
+) <= 3
+ORDER BY
+d.name, e.salary DESC;
+
+
+-- with highest_earner as(
+--     select d.name as Department,e.name as Employee,e.salary as Salary,
+--     dense_rank() over (partition by d.name order by e.salary desc) as rnk
+--     from Employee e
+--     left join Department d
+--     on e.departmentId=d.id    
+-- )
+-- select Department,Employee,Salary from highest_earner
+-- where rnk<=3;
 
 
 
