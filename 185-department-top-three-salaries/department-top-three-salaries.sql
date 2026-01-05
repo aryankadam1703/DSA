3b59640fcda5b9d4cd47f486b9a705059be7299d
Aryan Kadam <126709432+aryankadam1703@users.noreply.github.com>
2026-01-05 21:50:08 +0530

    Time: 900 ms (55.38%) | Memory: 0B (100.00%) - LeetSync

---
+++ 185-department-top-three-salaries/department-top-three-salaries.sql
@@ -0,0 +1,18 @@
+# Write your MySQL query statement below
+
+with highest_earner as(
+    select d.name as Department,e.name as Employee,e.salary as Salary,
+    dense_rank() over (partition by d.name order by e.salary desc) as rnk
+    from Employee e
+    left join Department d
+    on e.departmentId=d.id    
+)
+select Department,Employee,Salary from highest_earner
+where rnk<=3
+
+
+
+
+
+
+
