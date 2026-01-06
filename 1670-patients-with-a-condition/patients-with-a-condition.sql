d44353638d5a6fa53cb4f0730c367b1c05d6392a
Aryan Kadam <126709432+aryankadam1703@users.noreply.github.com>
2026-01-06 22:09:19 +0530

    Time: 406 ms (14.84%) | Memory: 0B (100.00%) - LeetSync

--- 1670-patients-with-a-condition/patients-with-a-condition.sql
+++ 1670-patients-with-a-condition/patients-with-a-condition.sql
@@ -1,4 +1,7 @@
 # Write your MySQL query statement below
+
+-- SELECT * FROM patients WHERE conditions REGEXP '\\bDIAB1'
+
 select * from patients
 where conditions like '% DIAB1%' or conditions like 'DIAB1%'
 order by patient_id
