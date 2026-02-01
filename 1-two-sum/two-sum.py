7d4809c93d9539a8baf90e3bff7caa151e36526d
Aryan Kadam <126709432+aryankadam1703@users.noreply.github.com>
2026-02-01 21:15:10 +0530

    Time: 2151 ms (20.69%) | Memory: 13.5 MB (17.49%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -5,17 +5,8 @@ class Solution(object):
         :type target: int
         :rtype: List[int]
         """
-        #Brute Force
-        # for i in range(0,len(nums)-1):
-        #     for j in range(i+1,len(nums)):
-        #         if nums[i]+nums[j]==target:
-        #             return [i,j]
-
-        #Hashtable
-
-        dict={}
-        for i,num in enumerate(nums):
-            difference=target-num
-            if difference in dict:
-                return [dict[difference],i]
-            dict[num]=i
+        for i in range(0,len(nums)-1):
+            for j in range(i+1,len(nums)):
+                if nums[i]+nums[j] == target :
+                    return [i,j]
+        
