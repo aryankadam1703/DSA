0094c18946dd06b74f40ccbfbc5b78d7436658af
Aryan Kadam <126709432+aryankadam1703@users.noreply.github.com>
2026-02-02 22:39:28 +0530

    Time: 7 ms (77.49%) | Memory: 12.4 MB (55.18%) - LeetSync

--- 9-palindrome-number/palindrome-number.py
+++ 9-palindrome-number/palindrome-number.py
@@ -4,10 +4,10 @@ class Solution(object):
         :type x: int
         :rtype: bool
         """
-        x=str(x)
-        if x == x[::-1]:
-            return True
-        return False
+        # x=str(x)
+        # if x == x[::-1]:
+        #     return True
+        # return False
 
-        # check = lambda val: False if val < 0 else str(val) == str(val)[::-1]
-        # return check(x)
+        check = lambda val: False if val < 0 else str(val) == str(val)[::-1]
+        return check(x)
