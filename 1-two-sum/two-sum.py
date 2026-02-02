cfefb4ae7296c4bf848cf76f8a08b9957441ede3
Aryan Kadam <126709432+aryankadam1703@users.noreply.github.com>
2026-02-02 21:36:09 +0530

    Time: 0 ms (100.00%) | Memory: 13.1 MB (79.40%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -5,7 +5,15 @@ class Solution(object):
         :type target: int
         :rtype: List[int]
         """
-        for i in range(len(nums)-1):
-            for j in range(i+1,len(nums)):
-                if nums[i]+nums[j] == target:
-                    return [i,j]
+        # Inefficient
+        # for i in range(len(nums)-1):                                  # Time complexity O(n^2)
+        #     for j in range(i+1,len(nums)):                            # Space Complexity O(1)
+        #         if nums[i]+nums[j] == target:
+        #             return [i,j]
+
+        num_map = {}
+        for i,num in enumerate(nums):              # Enumerate return pairs of (index,value)
+            complement = target-num
+            if complement in num_map:
+                return [num_map[complement],i]      # Here i is index of num(which is iterating in the nums list)
+            num_map[num] = i
