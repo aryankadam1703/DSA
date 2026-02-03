43bc4b18954bb5c0cf4866999b7ad274004d1945
Aryan Kadam <126709432+aryankadam1703@users.noreply.github.com>
2026-02-04 00:43:46 +0530

    Time: 14 ms (28.17%) | Memory: 12.4 MB (89.00%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.py
+++ 13-roman-to-integer/roman-to-integer.py
@@ -6,14 +6,21 @@ class Solution(object):
         """
         roman_num = {'I':1,'V':5,'X':10,'L':50,'C':100,'D':500,'M':1000}
         t=0
-        prev = 0
-        for char in reversed(s):
-            curr = roman_num[char]
-            if curr < prev:
-                t-=curr
+        # prev = 0
+        # for char in reversed(s):
+        #     curr = roman_num[char]
+        #     if curr < prev:
+        #         t-=curr
+        #     else:
+        #         t+=curr
+        #         prev = curr
+        # return t
+
+        for i in range(len(s)):
+            if i+1<len(s) and roman_num[s[i]] < roman_num[s[i+1]]:
+                t-=roman_num[s[i]]
             else:
-                t+=curr
-                prev = curr
-        return t
+                t+=roman_num[s[i]]
+        return t           
 
         
