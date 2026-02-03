ead8c2d00b8d47218c78f08f83e66d44f78d070d
Aryan Kadam <126709432+aryankadam1703@users.noreply.github.com>
2026-02-04 00:15:55 +0530

    Time: 6 ms (76.61%) | Memory: 12.6 MB (20.29%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.py
+++ 13-roman-to-integer/roman-to-integer.py
@@ -5,12 +5,8 @@ class Solution(object):
         :rtype: int
         """
         roman_num = {'I':1,'V':5,'X':10,'L':50,'C':100,'D':500,'M':1000}
-        total=0
+        t=0
         for i in range(len(s)):
-            if i+1<len(s) and roman_num[s[i]] < roman_num[s[i+1]]:
-                total -= roman_num[s[i]]
-            else:
-                total += roman_num[s[i]]
-        return total
-
+            t = t-roman_num[s[i]] if i+1<len(s) and roman_num[s[i]] < roman_num[s[i+1]] else t+roman_num[s[i]]
+        return t
         
