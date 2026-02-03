fa858ae82318c31d17b47ea95f601d51fcfd8483
Aryan Kadam <126709432+aryankadam1703@users.noreply.github.com>
2026-02-03 23:43:47 +0530

    Time: 7 ms (72.80%) | Memory: 12.5 MB (20.29%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.py
+++ 13-roman-to-integer/roman-to-integer.py
@@ -4,15 +4,13 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
-        dict={'I':1,'V':5,'X':10,'L':50,'C':100,'D':500,'M':1000}
-        l=len(s)
-        sum=0
-        prev=0
-        for i in reversed(s):
-            curr=dict[i]
-            if curr>=prev:
-                sum+=curr
+        roman_num = {'I':1,'V':5,'X':10,'L':50,'C':100,'D':500,'M':1000}
+        total=0
+        for i in range(len(s)):
+            if i+1<len(s) and roman_num[s[i]] < roman_num[s[i+1]]:
+                total -= roman_num[s[i]]
             else:
-                sum-=curr
-            prev=curr
-        return sum
+                total += roman_num[s[i]]
+        return total
+
+        
