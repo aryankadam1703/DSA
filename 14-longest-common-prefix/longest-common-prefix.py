d0a27b8154d43b41927265d0720d51c5af353d82
Aryan Kadam <126709432+aryankadam1703@users.noreply.github.com>
2026-02-06 23:20:06 +0530

    Time: 2 ms (38.34%) | Memory: 12.6 MB (36.08%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.py
+++ 14-longest-common-prefix/longest-common-prefix.py
@@ -4,16 +4,17 @@ class Solution(object):
         :type strs: List[str]
         :rtype: str
         """
-        output=""
-        s=min(strs,key=len)
-        for i in range(len(s)):
-            count=0
+
+        output = ''
+        min_s = min(strs,key=len)
+        for ch in range(len(min_s)):
+            count = 0
             for j in strs:
-                if s[i]==j[i]:
+                if min_s[ch] == j[ch]:
                     count+=1
-            if count==len(strs):
-                output+=s[i]
+            
+            if count == len(strs):
+                output += min_s[ch]
             else:
                 break
         return output
-        
